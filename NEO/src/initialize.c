25a4f4e6a4665964cb8d6028734732b018af2dfc
ricchiut <57751063+ricchiut@users.noreply.github.com>
2021-09-30 12:03:45 +0200

    Update initialize.c

--- NEO/src/initialize.c
+++ NEO/src/initialize.c
@@ -4,9 +4,7 @@
 This is initialize: it initializes some model, geometry, scheme and case
                       dependent constant and parameters
                              -------------------
-    begin                : Wed Apr 24 2002
-    copyright            : (C) 2002 by Mario Ricchiuto
-    email                : ricchiut@vki.ac.be
+    Code developed by M. Ricchiuto, Inria - mario.ricchiuto@inria.fr
  ***************************************************************************/
 #include "common.h"
 
@@ -77,13 +75,3 @@ void initialize()
 	
 }
 
-
-
-/***************************************************************************
- *                                                                         *
- *   This program has been developed at the von Karman Institute for Fluid *
- *   Dynamics by Mario Ricchiuto. Any change or update must be done in a   *
- *   clean way and it must be clearly commented.                           *
- *   Mario Ricchiuto                                                       *
- *                                              23/04/2002                 *
- ***************************************************************************/
