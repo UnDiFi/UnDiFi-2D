2df6f80745c62ec17fce9dd33925119c308ba0be
ricchiut <57751063+ricchiut@users.noreply.github.com>
2021-09-30 12:07:48 +0200

    Update nodal_residuals.c

--- NEO/src/nodal_residuals.c
+++ NEO/src/nodal_residuals.c
@@ -2,11 +2,9 @@
                               nodal_residuals.c
                               -----------------
 This is nodal_residuals: it performs the computation of the nodal resusuals
-    in pseudo time by looping over all the elements of the spatial mesh
+                       within the main element loop 
                              -------------------
-    begin                : Fri May 10 2002
-    copyright            : (C) 2002 by Mario Ricchiuto
-    email                : ricchiut@vki.ac.be
+    Code developed by M. Ricchiuto, Inria - mario.ricchiuto@inria.fr
  ***************************************************************************/
 #include "common.h"
 
@@ -58,11 +56,3 @@ void nodal_residuals()
 }
 
 
-/***************************************************************************
- *                                                                         *
- *   This program has been developed at the von Karman Institute for Fluid *
- *   Dynamics by Mario Ricchiuto. Any change or update must be done in a   *
- *   clean way and it must be clearly commented.                           *
- *   Mario Ricchiuto                                                       *
- *                                              23/04/2002                 *
- ***************************************************************************/
