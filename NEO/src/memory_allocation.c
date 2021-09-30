927e3a24eb0e287633859c01699f47712f0acf23
ricchiut <57751063+ricchiut@users.noreply.github.com>
2021-09-30 12:06:03 +0200

    Update memory_allocation.c

--- NEO/src/memory_allocation.c
+++ NEO/src/memory_allocation.c
@@ -2,11 +2,9 @@
                               memory_allocation.c
                               ------------------
                           This is memory_allocation:
-               any vector or matrix used in the code was born here
-                             --------------------
-    begin                : Tue Apr 30 2002
-    copyright            : (C) 2002 by Mario Ricchiuto
-    email                : ricchiut@vki.ac.be
+               dynamically allocated vector or matrices are created here
+                             -------------------
+    Code developed by M. Ricchiuto, Inria - mario.ricchiuto@inria.fr
  ***************************************************************************/
 #include "common.h"
 
@@ -260,11 +258,3 @@ double ***MA_double_tensor( long nx, long ny, long nz )
 	return m ;
 }
 
-/***************************************************************************
- *                                                                         *
- *   This program has been developed at the von Karman Institute for Fluid *
- *   Dynamics by Mario Ricchiuto. Any change or update must be done in a   *
- *   clean way and it must be clearly commented.                           *
- *   Mario Ricchiuto                                                       *
- *                                              23/04/2002                 *
- ***************************************************************************/
