345d73ddb81346bd1075e21cda2d9bf36f19d091
Lorenzo Campoli <campoli.lorenzo@gmail.com>
2026-08-09 16:15:41 +1000

    included mod_mesh.f90

--- source/mod_mesh.f90
+++ source/mod_mesh.f90
@@ -11,10 +11,10 @@ module mod_mesh
   public :: mesh_t
 
   type :: mesh_t
-    integer(i4) :: npoin = 0, nelem = 0, nbfac = 0, nbpoin = 0
-    integer(i4) :: nedge = 0, nhole = 0, nvt = 3, nclr = 0, npnod = 0
-    real(wp), allocatable :: xy(:, :)      ! (ndim, npoin)
-    real(wp), allocatable :: zroe(:, :)    ! (ndof, npoin)
+    integer(i4)              :: npoin = 0, nelem = 0, nbfac = 0, nbpoin = 0
+    integer(i4)              :: nedge = 0, nhole = 0, nvt = 3, nclr = 0, npnod = 0
+    real(wp),    allocatable :: xy(:, :)     ! (ndim, npoin)
+    real(wp),    allocatable :: zroe(:, :)   ! (ndof, npoin)
     integer(i4), allocatable :: celnod(:, :) ! (nvt, nelem)
     integer(i4), allocatable :: celcel(:, :) ! (nvt, nelem)
     integer(i4), allocatable :: bndfac(:, :) ! (3, nbfac)
@@ -25,8 +25,8 @@ module mod_mesh
     integer(i4), allocatable :: pmap(:)      ! (npoin), periodic-node map, see readpmap.f90
   contains
     procedure :: alloc => mesh_alloc
-    procedure :: free => mesh_free
-    final :: mesh_finalize
+    procedure :: free  => mesh_free
+    final     :: mesh_finalize
   end type mesh_t
 
   ! No copy_from: plain intrinsic assignment (bak = bkg) already deep-copies
@@ -37,23 +37,23 @@ contains
 
   subroutine mesh_alloc(this, ndim, ndof, npoin, nelem, nbfac, nvt, nedge)
     class(mesh_t), intent(inout) :: this
-    integer(i4), intent(in) :: ndim, ndof, npoin, nelem, nbfac, nvt, nedge
+    integer(i4),   intent(in)    :: ndim, ndof, npoin, nelem, nbfac, nvt, nedge
 
     call this%free()
 
     this%npoin = npoin
     this%nelem = nelem
     this%nbfac = nbfac
-    this%nvt = nvt
+    this%nvt   = nvt
     this%nedge = nedge
 
-    allocate (this%xy(ndim, npoin), source=0.0_wp)
-    allocate (this%zroe(ndof, npoin), source=0.0_wp)
+    allocate (this%xy(ndim, npoin),    source=0.0_wp)
+    allocate (this%zroe(ndof, npoin),  source=0.0_wp)
     allocate (this%celnod(nvt, nelem), source=0_i4)
     allocate (this%celcel(nvt, nelem), source=0_i4)
-    allocate (this%bndfac(3, nbfac), source=0_i4)
-    allocate (this%nodcod(npoin), source=0_i4)
-    allocate (this%edgptr(3, nedge), source=0_i4)
+    allocate (this%bndfac(3, nbfac),   source=0_i4)
+    allocate (this%nodcod(npoin),      source=0_i4)
+    allocate (this%edgptr(3, nedge),   source=0_i4)
   end subroutine mesh_alloc
 
   subroutine mesh_free(this)
@@ -62,18 +62,18 @@ contains
     this%npoin = 0; this%nelem = 0; this%nbfac = 0; this%nbpoin = 0
     this%nedge = 0; this%nhole = 0; this%nvt = 3; this%nclr = 0; this%npnod = 0
 
-    if (allocated(this%xy)) deallocate (this%xy)
-    if (allocated(this%zroe)) deallocate (this%zroe)
+    if (allocated(this%xy))     deallocate (this%xy)
+    if (allocated(this%zroe))   deallocate (this%zroe)
     if (allocated(this%celnod)) deallocate (this%celnod)
     if (allocated(this%celcel)) deallocate (this%celcel)
     if (allocated(this%bndfac)) deallocate (this%bndfac)
     if (allocated(this%nodcod)) deallocate (this%nodcod)
     if (allocated(this%nodptr)) deallocate (this%nodptr)
     if (allocated(this%edgptr)) deallocate (this%edgptr)
-    if (allocated(this%ia)) deallocate (this%ia)
-    if (allocated(this%ja)) deallocate (this%ja)
-    if (allocated(this%iclr)) deallocate (this%iclr)
-    if (allocated(this%pmap)) deallocate (this%pmap)
+    if (allocated(this%ia))     deallocate (this%ia)
+    if (allocated(this%ja))     deallocate (this%ja)
+    if (allocated(this%iclr))   deallocate (this%iclr)
+    if (allocated(this%pmap))   deallocate (this%pmap)
   end subroutine mesh_free
 
   subroutine mesh_finalize(this)
